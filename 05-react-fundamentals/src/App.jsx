30d2f635bfa2426f7260e762187fa4cac0a1f7f1
nnetraga97 <nikhil_netra@hotmail.com>
2026-07-05 16:09:42 -0700

    Persist users to localStorage with useEffect

--- 05-react-fundamentals/src/App.jsx
+++ 05-react-fundamentals/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import ProfileCard from './comp/ProfileCard'
 import SignUp from './comp/wizards/SignUp'
